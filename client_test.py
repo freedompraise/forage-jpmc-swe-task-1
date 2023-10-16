1c74a3caa6007eea9f05d3de0a098f05b4c60141
Praise Freedom Dike <dikepraise119@gmail.com>
2023-10-16 11:24:17 +0100

    write tests

--- client_test.py
+++ client_test.py
@@ -8,6 +8,10 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+      stock, bid_price, ask_price, price = getDataPoint(quote)
+      self.assertEqual(price, (bid_price + ask_price)/2)
+
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
@@ -16,9 +20,32 @@ class ClientTest(unittest.TestCase):
     ]
     """ ------------ Add the assertion below ------------ """
 
+    for quote in quotes:
+      stock, bid_price, ask_price, price = getDataPoint(quote)
+      self.assertEqual(price, (bid_price + ask_price)/2)
+
 
   """ ------------ Add more unit tests ------------ """
 
+  def test_getRatio_calculateRatio(self):
+    price_a = 100
+    price_b = 50
+    self.assertEqual(getRatio(price_a, price_b), 2)
+
+  def test_getRatio_calculateRatioPriceBZero(self):
+    price_a = 100
+    price_b = 0
+    self.assertIsNone(getRatio(price_a, price_b))
+
+  def test_getRatio_calculateRatioPriceAZero(self):
+    price_a = 0
+    price_b = 100
+    self.assertEqual(getRatio(price_a, price_b), 0)
+
+  def test_getRatio_calculateRatioPriceAZeroPriceBZero(self):
+    price_a = 0
+    price_b = 0
+    self.assertIsNone(getRatio(price_a, price_b))
 
 
 if __name__ == '__main__':
